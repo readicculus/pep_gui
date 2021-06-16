c629fba1f25aec2ed7b11e589437e0a151ee2b85
readicculus <yuval@yuvalboss.com>
2021-06-16 15:11:32 -0700

    windows compatibility fixes

--- pep_tk/core/util/env.py
+++ pep_tk/core/util/env.py
@@ -15,13 +15,16 @@ def shell_source(setup_viame_path):
     Runs setup_viame.sh and returns the below environment variables produced in the subprocess which can then
     be set for the current or other processes that use viame or kwiver apis.
     '''
-    # TODO: windows compatibility
     import subprocess
     kwiver_env_variables = ['PYTHONPATH', 'PYTHON_LIBRARY', 'QT_PLUGIN_PATH', 'VG_PLUGIN_PATH', 'VIDTK_MODULE_PATH',
                             'SPROKIT_MODULE_PATH', 'KWIVER_PLUGIN_PATH', 'VIAME_INSTALL', 'SPROKIT_PYTHON_MODULES',
                             'KWIVER_DEFAULT_LOG_LEVEL', 'CUDA_INSTALL_DIR', 'LD_LIBRARY_PATH', 'PATH']
-    pipe = subprocess.Popen("source %s; env" % setup_viame_path, stdout=subprocess.PIPE, shell=True,
-                            executable="/bin/bash")
+    if os.name == 'nt': # Windows
+        pipe = subprocess.Popen("%s; && SET" % setup_viame_path, stdout=subprocess.PIPE, shell=True)
+    else: # Linux
+        pipe = subprocess.Popen("source %s; env" % setup_viame_path, stdout=subprocess.PIPE, shell=True,
+                                executable="/bin/bash")
+
     output = pipe.communicate()[0].decode('utf-8').split('\n')
     env_kwiver = {}
     for o in output:
@@ -30,3 +33,5 @@ def shell_source(setup_viame_path):
             if kv[0] in kwiver_env_variables:
                 env_kwiver[kv[0]] = kv[1]
     return env_kwiver
+
+
--- pep_tk/psg/windows/initial_setup.py
+++ pep_tk/psg/windows/initial_setup.py
@@ -33,7 +33,8 @@ def initial_setup(skip_if_complete = True):
             break
         elif event == 'Complete Setup':
             selected_viame_filepath = values['-setup_viame_filepath-IN-']
-            if os.path.isfile(os.path.join(selected_viame_filepath, 'setup_viame.sh')):
+            if os.path.isfile(os.path.join(selected_viame_filepath, 'setup_viame.sh')) or \
+                os.path.isfile(os.path.join(selected_viame_filepath, 'setup_viame.bat')):
                 gui_settings[SettingsNames.setup_viame_filepath] = selected_viame_filepath
 
             selected_dataset_manifest_filepath = values['-dataset_manifest_filepath-IN-']
