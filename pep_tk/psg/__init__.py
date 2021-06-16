65bdc32fd5a091c3d383b1d8b18b193b5e16e057
readicculus <yuval@yuvalboss.com>
2021-06-16 14:09:30 -0700

    refactor/remove old code

--- pep_tk/psg/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-
-# PLUGIN_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# local_site_packages = os.path.join(PLUGIN_PATH, 'lib', 'python3.6', 'site-packages')
-# if not local_site_packages in sys.path:
-#     sys.path.insert(0, local_site_packages)
