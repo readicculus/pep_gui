ac9b0508bce167a7622428943226371d18ba0457
readicculus <yuval@yuvalboss.com>
2021-07-26 15:38:56 -0700

    import bug

--- launch.py
+++ launch.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-from core.parser import DatasetManifestError
 
 PLUGIN_PATH = os.path.dirname(os.path.abspath(__file__))
 plugin_python_lib = os.path.join(PLUGIN_PATH, 'lib', 'python3.6', 'site-packages')
 sys.path.insert(0, plugin_python_lib)
 from pep_tk.psg.windows.create_job import launch_gui
+from pep_tk.core.parser import DatasetManifestError
 
 def main():
     try:
