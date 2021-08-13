ad79910075ef58404ae9b5cef8f1b20e217939e5
readicculus <yuval@yuvalboss.com>
2021-08-12 17:21:59 -0700

    update veersioning

--- src/pep_tk/__init__.py
+++ src/pep_tk/__init__.py
@@ -1,4 +1,11 @@
 import os
-__version__ = "1.0.0"
+__version__ = "N/A"
 PLUGIN_PATH = os.path.normpath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
+version_txt_fp = os.path.join(os.path.dirname(__file__), 'VERSION.txt')
+with open(version_txt_fp, 'r') as file:
+    v_data = file.read().replace('\n', '')
+if len(v_data.split('.')) == 3:
+    __version__ = v_data
+else:
+    print(f'Invalid VERSION.cfg - could not parse {v_data}')
