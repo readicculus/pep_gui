588470c55fa2264de5c2ffcc75fb285b52ed6887
readicculus <yuval@yuvalboss.com>
2021-08-30 15:52:19 -0700

    license

--- src/launch.py
+++ src/launch.py
@@ -1,6 +1,18 @@
-import os
-import sys
-
+#     This file is part of the PEP GUI detection pipeline batch running tool
+#     Copyright (C) 2021 Yuval Boss yuval@uw.edu
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 def main():
     from pep_tk.core.parser import load_dataset_manifest, DatasetManifestError, EmptyParser
     from pep_tk.psg.settings import UserProperties
